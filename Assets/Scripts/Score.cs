c1de7c22d7cadb47f7eb40d3c288aac2988bb289
Luke King <king2lc@mail.uc.edu>
2021-04-20 16:18:12 -0400

    redacted fix
    fix the size

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,17 +6,6 @@ public class Score : MonoBehaviour
 {
     public float score;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnDestroy()
     {
         score = score + 1;
